d2f108784159a871cc3121992b3864f783e110ca
Parul bhugra <parulbhugra2@gmail.com>
2026-08-20 23:20:04 +0530

    changes

--- 03_Rockpaperscissors/game.py
+++ 03_Rockpaperscissors/game.py
@@ -1,21 +1,21 @@
 import random
-
+ 
 choices = ["rock", "paper", "scissors"]
 user_score = 0
 computer_score = 0
-
+ 
 while True:
     user_choice = input("Choose rock, paper, or scissors: ").lower()
-
+ 
     if user_choice not in choices:
         print("Invalid choice, try again.")
         continue
-
+ 
     computer_choice = random.choice(choices)
-
+ 
     print("You chose:", user_choice)
     print("Computer chose:", computer_choice)
-
+ 
     if user_choice == computer_choice:
         print("It's a tie!")
     elif (user_choice == "rock" and computer_choice == "scissors") or \
@@ -26,11 +26,11 @@ while True:
     else:
         print("Computer wins!")
         computer_score += 1
-
-    print("Score -> You:", user_score, " Computer:", computer_score)
-
+ 
+    print("Score ->", "You:", user_score, " Computer:", computer_score)
+ 
     play_again = input("Play again? (y/n): ").lower()
     if play_again != "y":
         break
-
-print("Thanks for playing!");
+ 
+print("Thanks for playing!")
